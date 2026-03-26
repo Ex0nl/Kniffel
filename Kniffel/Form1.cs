096d0456ee38c7bd21e66c9a41eff587dcb47dd7
Ex0nl <exonlgm@gmail.com>
2026-03-26 13:17:36 +0100

    test commit

--- Kniffel/Form1.cs
+++ Kniffel/Form1.cs
@@ -6,5 +6,10 @@ namespace Kniffel
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int test = 0;
+        }
     }
 }
